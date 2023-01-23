b967f12713303fc46e6f2eb51c508e248cd74ba7
gagip <y02645@gmail.com>
2023-01-24 00:35:12 +0900

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+    
+
+setuptools.setup(
+    name ='crawling-gagip',
+    version = '0.0.4',
+    description = 'crawling',
+    author = 'gagip',
+    author_email = 'y02645@gmail.com',
+    url = '',
+    py_modules = ['driver'],
+    packages=['crawling_gagip']
+)
