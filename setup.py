bf66725b6510c8ca766e8e23c05e97efc33b401c
gagip <y02645@gmail.com>
2023-01-24 01:01:28 +0900

    0.0.5 업데이트 - 주석 추가 - 불필요한 기능 제거

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ import setuptools
 
 setuptools.setup(
     name ='crawling-gagip',
-    version = '0.0.4',
+    version = '0.0.5',
     description = 'crawling',
     author = 'gagip',
     author_email = 'y02645@gmail.com',
     url = 'https://github.com/gagip/crawling-gagip',
     py_modules = ['driver'],
-    packages=['crawling_gagip']
+    packages=['crawling_gagip'],
+    python_requires='>=3.8',
 )
